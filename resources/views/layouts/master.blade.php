49986b7b316f8982f76de25c0e861bd14e1d0f2e
harkani <harkani.komputer@gmail.com>
2019-02-06 13:32:58 +0800

    changing title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,7 +10,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>AdminLTE 3 | Starter</title>
+  <title>LaraStart</title>
 
   <link rel="stylesheet" href="/css/app.css">
 </head>
